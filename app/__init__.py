a8bd5bea5499608079b8947dad2c0b621c4d9f9f
Josephshitandi <jsphshtnd@gmail.com>
2020-10-24 10:29:42 +0300

    Add pitch form where user can input pitches

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,9 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
-
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -20,16 +18,12 @@ mail = Mail()
 
 simple = SimpleMDE()
 
-
-
 def create_app(config_name):
-
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
     
-
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
